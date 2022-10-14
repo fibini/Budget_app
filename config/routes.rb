268f1412004bb86fbb812184189d6e5c13eb9fb8
Fabien <fabienswift@gmail.com>
2022-10-14 12:12:24 -0400

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  resources :items
-  root "splashes#index"
+
   devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :groups do
     resources :entities
   end
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "splashes#index"
 end
