bc0a2d556d673808810b930fa2936e14ad48993f
Fabien <fabienswift@gmail.com>
2022-10-14 17:43:08 -0400

    fix styling and remove commented code

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,19 +1,14 @@
 class EntitiesController < ApplicationController
-  # before_action :set_entity, only: %i[show edit update destroy]
-
-  # GET /entities or /entities.json
   def index
     @group = current_user.groups.find(params[:group_id])
     @entities = @group.entities.order(created_at: :desc)
     @amount = @entities.sum(:amount)
   end
 
-  # GET /entities/new
   def new
     @entity = Entity.new
   end
 
-  # POST /entities or /entities.json
   def create
     @group = Group.find(params[:group_id])
     @entity = Entity.new(entity_params)
@@ -31,20 +26,6 @@ class EntitiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /entities/1 or /entities/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @entity.update(entity_params)
-  #       format.html { redirect_to entity_url(@entity), notice: "Entity was successfully updated." }
-  #       format.json { render :show, status: :ok, location: @entity }
-  #     else
-  #       format.html { render :edit, status: :unprocessable_entity }
-  #       format.json { render json: @entity.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # DELETE /entities/1 or /entities/1.json
   def destroy
     @entity.destroy
 
@@ -56,12 +37,6 @@ class EntitiesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_entity
-  #   @entity = Entity.find(params[:id])
-  # end
-
-  # Only allow a list of trusted parameters through.
   def entity_params
     params.require(:entity).permit(:name, :amount)
   end
