43fc35186ed23e47ae6e6e5c0827cf504939fd24
Fabien <fabienswift@gmail.com>
2022-10-14 12:37:36 -0400

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
+  
   devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :groups do
     resources :entities
   end
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
